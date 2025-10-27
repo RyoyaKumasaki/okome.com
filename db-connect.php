21249c7b73e157dca0d028af3a7527de790a7a42
2401387p01 <2401387@s.asojuku.ac.jp>
2025-10-27 12:17:25 +0900

    データベース接続先作成

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql326.phy.lolipop.lan';
+const DBNAME = 'LAA1607615-okome';
+const USER = 'LAA1607615';
+const PASS = 'okome';
+$connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+$pdo = new PDO($connect, USER, PASS);
+?>
