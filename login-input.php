b4caf5dce77635fba387c8da978cbc6ffd92b25c
2401364 <2401364@s.asojuku.ac.jp>
2025-10-29 11:55:34 +0900

    ろぐいん

--- login-input.php
+++ login-input.php
@@ -1,7 +1,22 @@
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
+<?php session_start(); require 'header.php'; require 'menu.php'; ?>
+<h2>ログイン</h2>
 <form action="login-output.php" method="post">
-ログイン名<input type="text" name="login"><br>
-パスワード<input type="password" name="password"><br>
+<table>
+<tr>
+<td>ログインID：</td>
+<td><input type="text" name="login" required></td>
+</tr>
+<tr>
+<td>パスワード：</td>
+<td><input type="password" name="password" required></td>
+</tr>
+</table>
+<p>
 <input type="submit" value="ログイン">
+</p>
 </form>
+<p>
+<a href="forgot-password.php">パスワードを忘れた方はこちら</a><br>
+<a href="signup.php">アカウント作成はこちら</a>
+</p>
+<?php require 'footer.php'; ?>
