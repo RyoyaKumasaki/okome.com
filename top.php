6d13952b5a9c06b0be6e5738dd3e3c7821445a95
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-04 12:37:54 +0900

    Update top.php

--- top.php
+++ top.php
@@ -1,3 +1,6 @@
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
+<form action="product.php"　method="post">
+<input type="num" name="product_id">
+</form>
 <?php require 'footer.php'; ?>
