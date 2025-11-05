af0b01b3e6621bb784a4067fa9d471ef1bc24d01
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-05 12:04:42 +0900

    Update customer-input.php

--- customer-input.php
+++ customer-input.php
@@ -2,12 +2,15 @@
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-$name = $address = $login = $password = '';
+$name = $address = $login = $password = $mail = $telephone_number '';
 if (isset($_SESSION['customer'])) {
+    $user_id = $_SESSION['customer']['user_id'];
+    $mail = $_SESSION['customer']['mail'];
     $name = $_SESSION['customer']['name'];
     $address = $_SESSION['customer']['address'];
-    $login = $_SESSION['customer']['login'];
+    $login_name = $_SESSION['customer']['login_name'];
     $password = $_SESSION['customer']['password'];
+
 }
 echo '<form action="customer-output.php" method="post">';
 echo '<table>';
@@ -23,6 +26,11 @@ echo '</td></tr>';
 echo '<tr><td>パスワード</td><td>';
 echo '<input type="text" name="password" value="',$password,'">';
 echo '</td></tr>';
+echo '<tr><td>メールアドレス</td><td>';
+echo '<input type="text" name="mail" value="',$mail,'">';
+echo '</td></tr>';
+echo '<tr><td>電話番号</td><td>';
+echo '<input type="text" name="telephone_number" value="',$telephone_number,'">';
 echo '</table>';
 echo '<input type="submit" value="確定">';
 echo '</form>';
