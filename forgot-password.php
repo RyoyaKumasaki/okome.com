9a3f23d5b0e7e9ebb2f26ad793599d3402f272f8
2401364 <2401364@s.asojuku.ac.jp>
2025-11-05 11:07:33 +0900

    パスワード変更メール送信

--- forgot-password.php
+++ forgot-password.php
@@ -1,21 +1,50 @@
-<?php
+<?php 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST['email'] ?? '';
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        
-        header("Location: mail-sent.php");
-        exit;
+        $token = bin2hex(random_bytes(16));
+        $reset_link = "https://deci.jp/2025/kaihatu/reset-password.php?token=" . $token;
+
+
+        $subject = "【サイト名】パスワード再設定のご案内";
+        $message = <<<EOT
+{$email} 様
+
+パスワード再設定のご依頼を受け付けました。
+下記のリンクをクリックして、パスワード再設定ページへお進みください。
+
+{$reset_link}
+
+※このリンクの有効期限は1時間です。
+※このメールに心当たりがない場合は破棄してください。
+
+---------------------------------
+サイト名運営事務局
+EOT;
+
+       $headers = "From: okome.com\r\n";
+       $headers .= "Return-Path: info@deci.jp\r\n"; 
+       $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        if (mail($email, $subject, $message, $headers)) {
+            header("Location: mail-sent.php");
+            exit;
+        } else {
+            $error = "メール送信に失敗しました。サーバー管理者にお問い合わせください。";
+        }
+
     } else {
         $error = "有効なメールアドレスを入力してください。";
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
+<?php 
+require 'header.php';
+require 'menu.php';
+?>
+
 <title>パスワード再設定</title>
 <style>
 body {
@@ -47,5 +76,5 @@ button {
 <form action="login.php" method="get">
     <br><button type="submit" style="color:blue">キャンセル</button>
 </form>
-</body>
-</html>
+
+<?php require 'footer.php'; ?>
