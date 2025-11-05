701dc33defd4bae34381186a90b5933549fb0fe3
RyoyaKumasaki <2401383@s.asojuku.ac.jp>
2025-11-05 12:44:02 +0900

    ログイン機能

--- login-output.php
+++ login-output.php
@@ -6,16 +6,23 @@ require 'menu.php';
 
 unset($_SESSION['customer']);
 
-$sql = $pdo->prepare('SELECT * FROM customer WHERE login = ?');
-$sql->execute([$login]);
+$sql = $pdo->prepare('SELECT * FROM customer_user WHERE login_name = ?');
+$sql->execute([$_POST['login_name']]);
 $customer = $sql->fetch(PDO::FETCH_ASSOC);
 
-if ($customer && password_verify($password, $customer['password'])) {
+if ($customer && password_verify($_POST['password'], $customer['password'])) {
     $_SESSION['customer'] = [
-        'id' => $customer['id'],
-        'name' => $customer['name'],
-        'address' => $customer['address'],
-        'login' => $customer['login']
+        // 'id' => $customer['id'],
+        // 'name' => $customer['name'],
+        // 'address' => $customer['address'],
+        // 'login' => $customer['login']
+        'user_id'=>$customer['user_id'],
+        'mail'=>$customer['mail'],
+        'password'=>$customer['password'],
+        'name'=>$customer['name'],
+        'address'=>$customer['address'],
+        'login_name'=>$customer['login_name'],
+        'telephone_number'=>$customer['telephone_number']
     ];
     echo 'ログイン成功!ようこそ ' . htmlspecialchars($customer['name'], ENT_QUOTES, 'UTF-8');
 } else {
