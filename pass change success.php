f0a314327980f829e1bf13aa7372f38f069374e9
2401347 <2401347@s.asojuku.ac.jp>
2025-11-06 11:48:00 +0900

    ko

---
+++ pass change success.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>パスワードの変更が完了しました。<br>
+    下記からログイン画面に戻りログインしてください。</p>
+    <form action="login-input.php">
+        <button type="submit">ログイン画面に戻る</button>
+    </form>
+</body>
+</html>
