9a8888122465d97851c9e3214dadac80bd7a7a06
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-07 10:33:04 +0900

    Update top.php

--- top.php
+++ top.php
@@ -2,5 +2,15 @@
 <?php require 'menu.php'; ?>
 <form action="product.php" method="post">
 <input type="num" name="product_id">
+<input type="submit" value="商品詳細へ">
 </form>
+<a href="mypage.php">
+    <img src="" alt="">
+</a>
+<h2>
+    ランキング
+</h2>
+<?php
+require_once 'db-connect.php';
+$sql = $pdo->prepare('select * from reviw order by rating desc limit 3');
 <?php require 'footer.php'; ?>
