cb799ac5bb25c9d63ec37e8f9327681c369cb4b3
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-10 11:58:21 +0900

    Update top.php

--- top.php
+++ top.php
@@ -4,7 +4,7 @@
 <input type="num" name="product_id">
 </form>
 
-<a href="mypage.php"><img src="img/guest.png" alt=""></a>
+<a href="mypage.php"><img src="img/guest.png" width="150px"></a>
 <?php require 'ranking.php'; ?>
 <?php require 'product.php'; ?>
 <?php require 'footer.php'; ?>
