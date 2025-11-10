5dfb9d31f7ef7737c95aa4da28b2856c5d06fc6f
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-10 11:35:32 +0900

    Update product.php

--- product.php
+++ product.php
@@ -1,7 +1,3 @@
-<?php require_once 'db-connect.php'; ?>
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
-<h2>商品一覧</h2>
 <?php
 $sql = $pdo->query('SELECT * FROM product');
 foreach($sql as $row){
