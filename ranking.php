e4f8a3d7cf9b0f41ef7fb99181f98f04b4f719ac
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-13 12:02:19 +0900

    Update ranking.php

--- ranking.php
+++ ranking.php
@@ -40,7 +40,7 @@ function displayStars($rating) {
     }
     // 半星
     if ($halfStar) {
-        $stars .= "\u{2BA}";
+        $stars .= '☆';
         }
     // 空星
     for ($i = 0; $i < $emptyStars; $i++) {
