050c95038151ea88a7cd72ea37ce1676afab0ee0
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-17 12:12:29 +0900

    Update stock-show.php

--- stock-show.php
+++ stock-show.php
@@ -3,4 +3,7 @@
 <h1>在庫一覧</h1>
 <a href="show-stock.php"><button>更新</button></a>
 <?php require 'stock-table.php'; ?>
+<a href="stock-add.php">
+    <button>商品追加</button>
+</a>
 <?php require_once 'footer.php'?>
