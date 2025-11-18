cfc68f1f5062d3a4fc54133a1a978bd7ba1f7f26
RyoyaKumasaki <2401383@s.asojuku.ac.jp>
2025-11-18 12:38:42 +0900

    css装飾（くま）
    css

--- ranking.php
+++ ranking.php
@@ -48,16 +48,35 @@ function displayStars($rating) {
     }
     return $stars;
 }
-
-foreach ($sql as $row) {
-    echo '<h3>' . htmlspecialchars($row['product_name']) . '</h3>';
-    echo '<img src="img/' . htmlspecialchars($row['product_picture']) . '" width="150px"><br>';
-    echo '<p>価格：' . htmlspecialchars($row['price']) . '円</p>';
-    echo '<p style="color: #FFD700;">評価：' . displayStars($row['avg_rating']) . ' (' . number_format($row['avg_rating'], 1) . ')</p>';    
-    echo '<form action="product-detail.php" method="post" style="display:inline;">';
-    echo '<input type="hidden" name="product_id" value="' . htmlspecialchars($row['product_id']) . '">';
-    echo '<input type="submit" value="商品詳細を見る">';
-    echo '<hr>';
-    echo '</form>';
-}
 ?>
+<?php foreach ($sql as $row) : ?>
+    <?php $ranking = 1; ?>
+    <div class="card mt-4">
+    <div class="card-content">
+        <div class="columns is-mobile is-vcentered">
+            <div class="column is-narrow">
+                <div class="image is-96x96">
+                    <img src="img/<?= htmlspecialchars($row['product_picture']); ?>">
+                </div>
+            </div>
+            
+            <div class="column">
+                <p class="title is-4"><?= $ranking; ?>位【<?= htmlspecialchars($row['product_name']); ?>】</p>
+                <p class="subtitle is-6">価格：<?= htmlspecialchars($row['price']); ?>円</p>
+                <div class="is-flex is-align-items-center">
+                    <p class="mr-2" style="color: #FFD700;"
+                    >評価:<?= displayStars($row['avg_rating']); ?>（<?=number_format($row['avg_rating'], 1); ?>）</p>
+                </div>
+            </div>
+
+            <div class="column is-narrow has-text-right">
+                <form action="product-detail.php" method="post" style="display:inline;">
+                    <input type="hidden" name="product_id" value="<?= htmlspecialchars($row['product_id']); ?>">
+                    <input type="submit" class="button is-primary" value="商品詳細を見る"><br>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+    <?php $ranking++; ?>
+<?php endforeach; ?>
