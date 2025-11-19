ae06c75873487afda433712065fdbc0b784d6e9f
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-19 09:48:50 +0900

    Update product-add-input.php

--- product-add-input.php
+++ product-add-input.php
@@ -14,6 +14,15 @@
     echo '<tr><td>値段</td><td>';
     echo '<input type="text" name="price" value="',$price,'">';
     echo '</td></tr>';
+    echo '<tr><td>説明</td><td>';
+    echo '<input type="text" name="description" value="',$description,'">';
+    echo '</td></tr>';
+    echo '<tr><td>商品画像URL</td><td>';
+    echo '<input type="text" name="image_url" value="',$image_url,'">';
+    echo '</td></tr>';
+    echo '<tr><td>生産者画像URL</td><td>';
+    echo '<input type="text" name="producer_image_url" value="',$producer_image_url,'">';
+    echo '</td></tr>';
     echo '<input type="submit" value="確定">';
     echo '</form>';
 ?>
