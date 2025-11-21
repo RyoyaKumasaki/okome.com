bd463c100e28a1142c5ae8a2b97357c0f74bd176
okamoto0922 <2301147@s.asojuku.ac.jp>
2025-11-21 09:53:56 +0900

    aa

---
+++ reviewcontroll.php
@@ -0,0 +1,16 @@
+<?php
+    require 'dbconnect.php';
+    require 'controllheader.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>レビュー管理画面</title>
+</head>
+<body>
+    <h2>レビュー管理画面</h2>
+    
+</body>
+</html>
