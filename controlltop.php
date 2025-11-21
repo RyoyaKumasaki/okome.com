cf6edcdcd41a59825a9508570cb91cde33ef8684
RyoyaKumasaki <2401383@s.asojuku.ac.jp>
2025-11-21 10:32:24 +0900

    ファイル追加

--- controlltop.php
+++ controlltop.php
@@ -1,25 +1,15 @@
 <?php session_start(); ?>
-<?php require 'dbconnect.php';
-      require 'controllheader.php';
-      require 'menu.php';
-      require 'nav.php';
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>管理者トップ画面</title>
-</head>
-<body>
+<?php require 'controllheader.php'; ?>
+<?php $page_title = '管理者トップ画面'; ?>
     <h2>管理画面</h2>
-    <form action="" method="post">
+    <form action="stock-show.php" method="post">
         <button type="submit">在庫管理</button>
     </form>
     <form action="" method="post">
         <button type="submit">レビュー管理</button>
     </form>
     <h2>アカウント検索</h2>
-    <form action="" method="post">
+    <form action="account-management.php" method="post">
     <input type="textbox" name="user_name" value="ユーザーID検索">
     <button type="submit">検索</button>
     </form>
