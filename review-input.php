f1acef9cbe1a02ab988aa293ec023e3b4c638272
2401387p01 <2401387@s.asojuku.ac.jp>
2025-11-25 12:18:05 +0900

    レビュー投稿ページの作成

---
+++ review-input.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+<?php require 'header.php'?>
+<?PHP require 'menu.php'?>
+<?php require 'db-connect.php' ?>
+<?php
+$sql = $pdo->prepare('select * from product where id = ?');
+$sql->execute([$_GET['id']]);
+$product = $sql->fetch();
+echo '<h2>商品レビュー</h2>';
+echo '<p>' . htmlspecialchars($product['product_picture'], ENT_QUOTES, 'UTF-8') . '</p>';
+echo '<p>商品名: ' . htmlspecialchars($product['product_name'], ENT_QUOTES, 'UTF-8') . '</p>';
+echo '<p>評価点</p>';   
+echo '<form action="review-output.php" method="post">';
+echo '<input type="hidden" name="product_id" value="' . htmlspecialchars($product['id'], ENT_QUOTES, 'UTF-8') . '">';
+for ($i = 1; $i <= 5; $i++) {
+    echo '<input type="radio" name="rating" value="' . $i . '">' . $i . ' ';
+}
+echo '<p>コメント</p>';
+echo '<textarea name="comment" rows="4" cols="40"></textarea></p>';
+echo '<input type="submit" value="投稿">';
+echo '</form>';
+?>
