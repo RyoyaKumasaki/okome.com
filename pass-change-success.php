08676b66aec342d6d06ed2355caf04fc45fdd4ca
RyoyaKumasaki <2401383@s.asojuku.ac.jp>
2025-12-03 10:41:08 +0900

    CSS装飾
    くま

--- pass-change-success.php
+++ pass-change-success.php
@@ -3,9 +3,27 @@ $page_title = 'パスワード変更完了';
 require 'header.php';
 require 'menu.php';
 ?>
-    <p>パスワードの変更が完了しました。<br>
-    下記からログイン画面に戻りログインしてください。</p>
-    <form action="login-input.php">
-        <button type="submit">ログイン画面に戻る</button>
-    </form>
+
+<div class="container is-max-desktop p-5 mt-5 has-background-white has-text-centered">
+    <div class="box">
+        <h1 class="title is-3 has-text-success mb-5">
+            <span class="icon is-large"><i class="fas fa-check-circle"></i></span>
+            <span>パスワードの変更が完了しました</span>
+        </h1>
+        
+        <p class="subtitle is-5 mb-5">
+            下記からログイン画面に戻り、新しいパスワードでログインしてください。
+        </p>
+
+        <hr>
+        
+        <form action="login-input.php" method="get">
+            <button type="submit" class="button is-primary is-large is-rounded">
+                <span class="icon"><i class="fas fa-sign-in-alt"></i></span>
+                <span>ログイン画面に戻る</span>
+            </button>
+        </form>
+    </div>
+</div>
+
 <?php require 'footer.php'; ?>
